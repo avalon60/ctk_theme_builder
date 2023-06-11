9449d3070ea7c238b7276ab26eaa46bb1b2d71af
Clive Bostock <cbostock20@gmail.com>
2023-06-11 15:28:40 +0100

    New QA feature

---
+++ ctk_theme_builder_qa_app.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+##############################################################################
+# Author: Clive Bostock
+#   Date: 10 June 2023
+#   Name: ctk_theme_builder_test_app.sh
+#  Descr: CustomTkinter Theme Builder Test App Launcher (Linux/Mac)
+##############################################################################
+PROG=`basename $0`
+PROG_PATH=`dirname $0`
+FULL_PATH=$0
+if [[ "${PROG}" == *\.sh ]]
+then
+  DCCM_PY=`echo ${FULL_PATH} | sed "s/\.sh/.py/"`
+else
+  DCCM_PY="${FULL_PATH}.py"
+fi
+APP_HOME=`echo ${FULL_PATH} | sed "s/${PROG}//; s/\/$//"`
+APP_ENV=${PROG_PATH}/venv
+if [ -f ${APP_ENV}/bin/activate ]
+then
+  source ${APP_ENV}/bin/activate
+elif [ -f ${APP_ENV}/Scripts/activate ]
+then
+  source ${APP_ENV}/Scripts/activate
+fi 
+type python 2> /dev/null
+if [ $? -eq 0 ]
+then
+  PYTHON="python"
+else
+  type python3 2> /dev/null
+  if [ $? -eq 0 ]
+  then
+    PYTHON="python3"
+  else
+    echo -e "Cannot find a Python interpreter. Please ensure that you have Python installed and that it can be found via \$PATH"
+    exit 1
+  fi
+fi
+${PYTHON} ${DCCM_PY} $*
+
