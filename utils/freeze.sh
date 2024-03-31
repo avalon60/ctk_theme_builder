5f7157c120f1ae3474ecaf414e963790db0ea888
Clive Bostock <cbostock20@gmail.com>
2024-03-31 16:10:10 +0100

    Relocated utility scripts/files to the new utils folder.

--- utils/freeze.sh
+++ utils/freeze.sh
@@ -4,5 +4,9 @@
 #   Name: freeze.bat
 #  Descr: Generates a Pyython requirements.txt file for DCCM
 ##############################################################################
+APP_HOME=$(realpath $0)
+APP_HOME=$(dirname ${APP_HOME})
+APP_HOME=$(dirname ${APP_HOME})
+cd ${APP_HOME}
 source venv/bin/activate
 pip freeze | grep -v "apt-clone" > requirements.txt
