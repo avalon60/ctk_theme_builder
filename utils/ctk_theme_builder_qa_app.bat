cf38cbaa24f46bc4954b0ccdac5a3c8186fa56cd
Clive Bostock <cbostock20@gmail.com>
2024-03-31 16:13:56 +0100

    Modified due to changes in the  component locations (MVC paradigm)

--- utils/ctk_theme_builder_qa_app.bat
+++ utils/ctk_theme_builder_qa_app.bat
@@ -5,7 +5,14 @@
 ::#  Descr:CustomTkinter Theme Builder Test App Launcher  (Windows)
 ::##############################################################################
 @echo off
-set PROG_PATH="%~dp0"
-set APP_ENV=%PROG_PATH%\venv
+:: Get the directory of the batch script
+set PROG_PATH=%~dp0
+
+:: Derive the parent directory of PROG_PATH
+for %%I in ("%PROG_PATH%\..") do set "APP_HOME=%%~fI"
+
+set APP_ENV=%APP_HOME%\venv
+set PYTHONPATH=%PYTHONPATH%;%APP_HOME%\utils;%APP_HOME%\model;%APP_HOME%\view
+
 call %APP_ENV%\Scripts\activate.bat
-%PROG_PATH%\ctk_theme_builder_qa_app.py %1 %2 %3 %4 %5 %6
+%APP_HOME%\utils\ctk_theme_builder_qa_app.py %1 %2 %3 %4 %5 %6
