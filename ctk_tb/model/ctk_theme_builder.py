3fe12d79533c747e8c48c577f8a12ea4dd9efe0d
avalon60 <cbostock20@gmail.com>
2026-04-17 19:43:16 +0100

    Version bump

--- ctk_tb/model/ctk_theme_builder.py
+++ ctk_tb/model/ctk_theme_builder.py
@@ -1,6 +1,6 @@
 __title__ = 'CB CustomTkinter Theme Builder Module'
 __author__ = 'Clive Bostock'
-__version__ = "3.2.14"
+__version__ = "3.2.15"
 __license__ = 'MIT - see LICENSE.md'
 
 import copy
